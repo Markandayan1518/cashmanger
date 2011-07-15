04117c0070419bd1cfc06d98ad34c186d5ea869d
k1k3t0 <k1k3t0@3ef5467b-eb12-fa92-938f-20641c5f893b>
2011-07-15 19:29:11 +0000

    Creazione package cashmanager.calendar. Creazione classi JMonthChooser, JYearChooser. Modifica classi CashManagerDB, Transaction, TransactionDialog. (Alan Bertoni)

---
+++ CashManager/src/cashmanager/calendar/JMonthChooser.java
@@ -0,0 +1,147 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cashmanager.calendar;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.text.DateFormatSymbols;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Locale;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Kiko
+ */
+public class JMonthChooser extends JPanel implements ItemListener{
+    private JComboBox monthBox;
+    private Locale locale;
+    private int currentMonth;
+    private boolean initialized;
+    private boolean localeInitialize;
+
+    public JMonthChooser(){
+        super();
+        setName("JMonthChooser");
+        setLayout(new BorderLayout());
+        monthBox = new JComboBox();
+        monthBox.addItemListener(this);
+        locale = Locale.getDefault();
+        initMonth();
+        add(monthBox, BorderLayout.CENTER);
+        initialized = true;
+        Calendar c = Calendar.getInstance(locale);
+        setMonth(c.get(Calendar.MONTH));
+    }
+
+    private final void initMonth(){
+        localeInitialize = true;
+        DateFormatSymbols date = new DateFormatSymbols(locale);
+        String[] months = date.getMonths();
+
+        if(monthBox.getItemCount() == 12){
+            monthBox.removeAllItems();
+        }
+        for(int i = 0; i < 12; i++){
+            monthBox.addItem(months[i]);
+        }
+        localeInitialize = false;
+        monthBox.setSelectedIndex(currentMonth);
+    }
+
+    public void setMonth(int newMonth){
+        setMonth(newMonth, true);
+    }
+    private void setMonth(int newMonth, boolean updateSelection){
+        if(!initialized || localeInitialize){
+            return;
+        }
+
+        int oldMonth = currentMonth;
+
+        if(newMonth < 0){
+            currentMonth = 0;
+        }else if(newMonth > 11){
+            currentMonth = 11;
+        }else{
+            currentMonth = newMonth;
+        }
+
+        if(updateSelection){
+            monthBox.setSelectedIndex(currentMonth);
+        }
+
+        firePropertyChange("month", oldMonth, currentMonth);
+    }
+    public void setLocale(Locale l){
+//        super.setLocale(l);
+        locale = l;
+        initMonth();
+    }
+
+    public int getMonth(){
+        return currentMonth;
+    }
+    public Locale getLocale(){
+        return locale;
+    }
+
+    public void itemStateChanged(ItemEvent e){
+        if(e.getStateChange() == ItemEvent.SELECTED){
+            int index = monthBox.getSelectedIndex();
+            if((index >= 0) && (index != currentMonth)){
+                setMonth(index, false);
+            }
+        }
+    }
+    
+    public static void main(String args[]){
+        JFrame frame = new JFrame("JMonthChooser");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JMonthChooser mc = new JMonthChooser();
+        Locale[] list = Locale.getAvailableLocales();
+        String[] a = new String[list.length];
+        int i = 0;
+        for(Locale l : list){
+            a[i] = l.getDisplayName();
+            i++;
+        }
+        Arrays.sort(a);
+        final JComboBox b = new JComboBox();
+//        for(Locale l : list){
+//            b.addItem(l.getDisplayName());
+//        }
+        for(String s : a){
+            b.addItem(s);
+        }
+        b.addItemListener(new ItemListener(){
+            public void itemStateChanged(ItemEvent e){
+                if(e.getStateChange() == ItemEvent.SELECTED){
+                    String sel = (String)b.getSelectedItem();
+                    Locale[] list = Locale.getAvailableLocales();
+                    for(Locale l : list){
+                        if(l.getDisplayName().equals(sel)){
+                            mc.setLocale(l);
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+        frame.add(mc, BorderLayout.CENTER);
+        frame.add(b, BorderLayout.PAGE_END);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}//JMonthChooser
---
+++ CashManager/src/cashmanager/calendar/JYearChooser.java
@@ -0,0 +1,181 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cashmanager.calendar;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.util.Calendar;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingConstants;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ *
+ * @author Kiko
+ */
+public class JYearChooser extends JPanel implements FocusListener, CaretListener, ActionListener, ChangeListener{
+
+    private JSpinner spinner;
+    private SpinnerNumberModel spinnerModel;
+    private JTextField textField;
+    private int minValue = Integer.MIN_VALUE;
+    private int maxValue = Integer.MAX_VALUE;
+    private int defaultValue;
+    private int currentValue;
+    private Color editColor = Color.GREEN;
+
+    public JYearChooser(){
+        this(Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    public JYearChooser(int min, int max){
+        super();
+        setName("JYearChooser");
+        setLayout(new BorderLayout());
+        setMinValue(min);
+        setMaxValue(max);
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        setDefaultValue(year);
+        setCurrentValue(defaultValue);
+        int step = 1;
+        spinnerModel = new SpinnerNumberModel(defaultValue, minValue, maxValue, step);
+        spinner = new JSpinner(spinnerModel);
+        spinner.addChangeListener(this);
+
+        textField = new JTextField();
+        textField.setHorizontalAlignment(SwingConstants.TRAILING);
+        setValue(defaultValue);
+        textField.addActionListener(this);
+        textField.addFocusListener(this);
+        textField.addCaretListener(this);
+        spinner.setEditor(textField);
+        add(spinner, BorderLayout.CENTER);
+
+        printInfo();
+    }
+
+    public final void setMinValue(int min){
+        if(min > maxValue){
+            minValue = maxValue;
+        }else{
+            minValue = min;
+        }
+    }
+    public final void setMaxValue(int max){
+        if(max < minValue){
+            maxValue = minValue;
+        }else{
+            maxValue = max;
+        }
+    }
+    public final void setDefaultValue(int defValue){
+        if(defValue < minValue){
+            defaultValue = minValue;
+        }else if(defValue > maxValue){
+            defaultValue = maxValue;
+        }else{
+            defaultValue = defValue;
+        }
+    }
+    public final void setCurrentValue(int currValue){
+        if(currValue < minValue){
+            currentValue = minValue;
+        }else if(currValue > maxValue){
+            currentValue = maxValue;
+        }else{
+            currentValue = currValue;
+        }
+    }
+
+    public void setValue(int value){
+        setValue(value, true);
+        spinner.setValue(value);
+        System.out.println((Integer)spinner.getValue());
+    }
+    public void setValue(int value, boolean updateTextField){
+        int oldValue = currentValue;
+        setCurrentValue(value);
+
+        if(updateTextField){
+            textField.setText(Integer.toString(value));
+            textField.setForeground(Color.BLACK);
+        }
+    }
+
+    public void printInfo(){
+        System.out.println(spinner.getPreferredSize());
+        System.out.println(spinner.getMinimumSize());
+        System.out.println(spinner.getMaximumSize());
+        System.out.println(getPreferredSize());
+        System.out.println(getMinimumSize());
+        System.out.println(getMaximumSize());
+    }
+
+    public void focusGained(FocusEvent e){
+    }
+
+    public void focusLost(FocusEvent e){
+//        actionPerformed(null);
+    }
+
+    public void caretUpdate(CaretEvent e){
+        try{
+            int testValue = Integer.valueOf(textField.getText()).intValue();
+            if((testValue >= minValue) && (testValue <= maxValue)){
+                textField.setForeground(editColor);
+//                setValue(testValue);
+            }else{
+                textField.setForeground(Color.red);
+            }
+        }catch(Exception ex){
+            if(ex instanceof NumberFormatException){
+                textField.setForeground(Color.red);
+            }
+            // Ignore all other exceptions, e.g. illegal state exception
+        }
+        textField.repaint();
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if(textField.getForeground().equals(editColor)){
+            setValue(Integer.parseInt(textField.getText()));
+            //Spara un evento per il listener
+        }
+    }
+
+    public void stateChanged(ChangeEvent e) {
+        if(spinnerModel instanceof SpinnerNumberModel){
+            int value = spinnerModel.getNumber().intValue();
+            setValue(value);
+        }
+    }
+
+    public static void main(String args[]){
+        JFrame frame = new JFrame("JYearChooser");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new JYearChooser(0, 9999));
+        frame.pack();
+        frame.setLocation(300, 300);
+        frame.setVisible(true);
+        System.out.println(frame);
+        System.out.println(frame.getPreferredSize());
+        System.out.println(frame.getMinimumSize());
+        System.out.println(frame.getMaximumSize());
+    }
+
+
+}//JYearChooser
